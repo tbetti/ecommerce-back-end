f108290716476ef95c83ed7eb1af78224be20d67
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-09-01 11:49:33 -0400

    Created routes to get all products & products by ID

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,13 +6,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      { model: Category },
+      { model: Tag }
+    ]
+  })
+  .then(product =>{
+    res.json(product);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [
+      { model: Category },
+      { model: Tag }
+    ]
+  })
+  .then(product =>{
+    res.json(product);
+  })
 });
 
 // create new product
